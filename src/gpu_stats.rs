3c1e72e3989c05f9e056ce273cfe84c460680769
Sausagerrito <rdoddridgea@gmail.com>
2025-06-23 22:16:07 -0400

    500ms update, gpu name, clock, and mem clock

--- src/gpu_stats.rs
+++ src/gpu_stats.rs
@@ -1,12 +1,19 @@
 use nvml_wrapper::Nvml;
 
-pub fn print_gpu(){
+pub fn print_gpu() -> (String, u32, u32, f64) {
     let nvml = Nvml::init().unwrap();
     let device = nvml.device_by_index(0).unwrap();
     let memory_info = device.memory_info().unwrap();
     let mem_used: f64 = memory_info.used as f64;
     let mem_tot: f64 = memory_info.total as f64;
+    let mem = mem_used / mem_tot;
 
-
-    println!("Memory Usage: {:.4}%", (mem_used/mem_tot));
+    let gpu_name = device.name().unwrap();
+    let gpu_clock = device
+        .clock_info(nvml_wrapper::enum_wrappers::device::Clock::Graphics)
+        .unwrap();
+    let mem_clock = device
+        .clock_info(nvml_wrapper::enum_wrappers::device::Clock::Memory)
+        .unwrap();
+    (gpu_name, gpu_clock, mem_clock, mem)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
+//use crossterm;
+use ratatui;
+use std;
 mod gpu_stats;
 
 fn main() {
-    gpu_stats::print_gpu();
+    let mut terminal = ratatui::init();
+
+    loop {
+        terminal.draw(draw).unwrap();
+        std::thread::sleep(std::time::Duration::from_millis(500));
+    }
+
+    //ratatui::restore();
+}
+
+pub fn draw(frame: &mut ratatui::Frame) {
+    let gpu_vec = gpu_stats::print_gpu();
+    let text = format!(
+        "GPU: {}    Core Clock: {} mHz    Memory Clock: {} mHz    VRAM Usage: {:.2}%",
+        gpu_vec.0, gpu_vec.1, gpu_vec.2, gpu_vec.3
+    );
+
+    frame.render_widget(text, frame.area());
 }
