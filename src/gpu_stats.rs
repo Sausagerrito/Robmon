7ea51516e698d9db6262dcc57306d3fbb005556b
Sausagerrito <rdoddridgea@gmail.com>
2025-06-24 14:18:26 -0400

    added bar charts

--- src/gpu_stats.rs
+++ src/gpu_stats.rs
@@ -1,12 +1,12 @@
 use nvml_wrapper::Nvml;
 
-pub fn print_gpu() -> (String, u32, u32, f64) {
+pub fn print_gpu() -> (String, u32, u32, f64, u32, u32) {
     let nvml = Nvml::init().unwrap();
     let device = nvml.device_by_index(0).unwrap();
     let memory_info = device.memory_info().unwrap();
     let mem_used: f64 = memory_info.used as f64;
     let mem_tot: f64 = memory_info.total as f64;
-    let mem = mem_used / mem_tot;
+    let mem = 100.00 * (mem_used / mem_tot);
 
     let gpu_name = device.name().unwrap();
     let gpu_clock = device
@@ -15,5 +15,8 @@ pub fn print_gpu() -> (String, u32, u32, f64) {
     let mem_clock = device
         .clock_info(nvml_wrapper::enum_wrappers::device::Clock::Memory)
         .unwrap();
-    (gpu_name, gpu_clock, mem_clock, mem)
+    let gpu_usage = device.utilization_rates().unwrap();
+    let core_usage = gpu_usage.gpu;
+    let bus_usage = gpu_usage.memory;
+    (gpu_name, gpu_clock, mem_clock, mem, core_usage, bus_usage)
 }
--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,73 @@ fn main() {
     let mut terminal = ratatui::init();
 
     loop {
-        terminal.draw(draw).unwrap();
+        let gpu_vec = gpu_stats::print_gpu();
+        terminal
+            .draw(|frame| {
+                draw(
+                    frame, &gpu_vec.0, &gpu_vec.1, &gpu_vec.2, gpu_vec.3, gpu_vec.4, &gpu_vec.5,
+                )
+            })
+            .unwrap();
         std::thread::sleep(std::time::Duration::from_millis(500));
     }
 
     //ratatui::restore();
 }
 
-pub fn draw(frame: &mut ratatui::Frame) {
-    let gpu_vec = gpu_stats::print_gpu();
-    let text = format!(
-        "GPU: {}    Core Clock: {} mHz    Memory Clock: {} mHz    VRAM Usage: {:.2}%",
-        gpu_vec.0, gpu_vec.1, gpu_vec.2, gpu_vec.3
-    );
+pub fn draw(
+    frame: &mut ratatui::Frame,
+    gpu_name: &str,
+    core_clock: &u32,
+    mem_clock: &u32,
+    vram_usage: f64,
+    core_usage: u32,
+    _bus_usage: &u32,
+) {
+    let outer_block = ratatui::widgets::Block::default()
+        .title(gpu_name)
+        .borders(ratatui::widgets::Borders::ALL);
+    let area = frame.area();
+    let inner_area = outer_block.inner(area);
+    frame.render_widget(outer_block, area);
 
-    frame.render_widget(text, frame.area());
+    // Horizontal Layout
+    let chunks = ratatui::layout::Layout::default()
+        .direction(ratatui::layout::Direction::Horizontal)
+        .constraints([
+            ratatui::layout::Constraint::Percentage(50),
+            ratatui::layout::Constraint::Percentage(50),
+        ])
+        .split(inner_area);
+
+    // Vertical Splits
+    // GPU CORE CLOCK CHART
+    // For core_chart:
+    let core_bar_width = chunks[0].width.saturating_sub(2); // subtract borders
+    let core_chart = ratatui::widgets::BarChart::default()
+        .block(
+            ratatui::widgets::Block::default()
+                .title(format!("C. CLK: {} MHz", core_clock))
+                .borders(ratatui::widgets::Borders::ALL),
+        )
+        .data(&[("LOAD:", u64::from(core_usage))])
+        .bar_width(core_bar_width)
+        .max(100)
+        .bar_style(ratatui::style::Style::default().fg(ratatui::style::Color::Green));
+
+    // For mem_chart:
+    let mem_bar_width = chunks[1].width.saturating_sub(2); // subtract borders
+    let mem_chart = ratatui::widgets::BarChart::default()
+        .block(
+            ratatui::widgets::Block::default()
+                .title(format!("M. CLK: {} MHz", mem_clock))
+                .borders(ratatui::widgets::Borders::ALL),
+        )
+        .data(&[("VRAM:", vram_usage as u64)])
+        .bar_width(mem_bar_width)
+        .max(100)
+        .bar_style(ratatui::style::Style::default().fg(ratatui::style::Color::Blue));
+
+    frame.render_widget(core_chart, chunks[0]);
+    frame.render_widget(mem_chart, chunks[1]);
 }
